7a206fdc1df22c08c82fec37307b5fdb79aff7bd
burack <omerfaruk.aksak@student.ucll.be>
2022-09-24 05:42:53 +0200

    init project

---
+++ src/main/java/com/example/verdandibackend/VerdandiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.verdandibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdandiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerdandiBackendApplication.class, args);
+    }
+
+}
